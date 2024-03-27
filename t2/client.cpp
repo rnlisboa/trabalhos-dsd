40be14c76bcad16381c7685a511e5f25ff2626a6
Renan Lisboa <104840232+rnlisboa@users.noreply.github.com>
2024-03-27 08:26:16 -0300

    Update client.cpp

--- t2/client.cpp
+++ t2/client.cpp
@@ -1,5 +1,3 @@
-// C++ program to illustrate the client application in the 
-// socket programming 
 #include <cstring> 
 #include <iostream> 
 #include <netinet/in.h> 
@@ -8,24 +6,19 @@
 
 int main() 
 { 
-	// creating socket 
 	int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
 
-	// specifying address 
 	sockaddr_in serverAddress; 
 	serverAddress.sin_family = AF_INET; 
 	serverAddress.sin_port = htons(8080); 
 	serverAddress.sin_addr.s_addr = INADDR_ANY; 
 
-	// sending connection request 
 	connect(clientSocket, (struct sockaddr*)&serverAddress, 
 			sizeof(serverAddress)); 
 
-	// sending data 
 	const char* message = "Hello, server!"; 
 	send(clientSocket, message, strlen(message), 0); 
 
-	// closing socket 
 	close(clientSocket); 
 
 	return 0; 
