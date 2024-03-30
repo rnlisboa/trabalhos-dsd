9513986fc29f852ce46e20a83bda2a079fe7930b
rnlisboa <dev.renanlisboa@gmail.com>
2024-03-30 19:30:25 -0300

    update

--- tcp-udp/tcp/server/createTcp.js
+++ tcp-udp/tcp/server/createTcp.js
@@ -1,45 +1,49 @@
 var net = require('net');
 
-var listarArquivos = require("./util")
+var listarArquivos = require("./listarArquivos")
+
+const caminhoDiretorio = "./arquivos/"
 
 var server = net.createServer(function (socket) {
-    console.log('Cliente conectado');
+	console.log('Cliente conectado');
 
-    socket.write("Bem vindo ao servidor.\nComandos:\n- /l - Listar arquivos do diretório.\n- /b - Baixar arquivo\r\n");
+	socket.write("Bem vindo ao servidor.\nentradas:\n- /l - Listar arquivos do diretório.\n- /b - Baixar arquivo\r\n");
 
-    socket.on('data', function (data) {
-        const comando = data.toString();
-        if (comando === '/l') {
-            console.log('Comando recebido:', comando);
-            socket.write("Listando os arquivos...\r\n");
-            listarArquivos(function (arquivos) {
-                arquivos.forEach(function (arquivo) {
-                    socket.write(arquivo + '\n');
-                });
-                socket.write('\r\n');
-            });
-        }
-
-        if (comando === '/b') {
-            console.log('Comando recebido:', comando);
-            socket.write('Selecione o arquivo que deseja baixar: \n');
-            listarArquivos(function (arquivos) {
+	socket.on('data', function (data) {
+		const entrada = data.toString();
+		if (entrada === '/l') {
+			console.log('entrada recebido:', entrada);
+			socket.write("Listando os arquivos...\r\n");
+			listarArquivos(function (arquivos) {
 				let arq = arquivos.join('\n')
-                
-                socket.write(`\n${arq}\r\n`);
-            });
-        }
-    });
 
-    socket.on('error', function (err) {
-        console.error('Erro no socket:', err.message);
-    });
+				socket.write(`\n${arq}\r\n`);
+			});
+		} else if (entrada === '/b') {
+			console.log('entrada recebido:', entrada);
+			socket.write('Selecione o arquivo que deseja baixar: \n');
+		} else {
+			listarArquivos(function (arquivos) {
+				if (arquivos.indexOf(entrada) == -1){
+					socket.write("Arquivo não encontrado\r\n");
+				} else {
+					socket.write(`${caminhoDiretorio}${entrada}`)
+				}
+			})
+		}
 
-    socket.on('close', function () {
-        console.log('Conexão fechada pelo cliente');
-    });
+
+	});
+
+	socket.on('error', function (err) {
+		console.error('Erro no socket:', err.message);
+	});
+
+	socket.on('close', function () {
+		console.log('Conexão fechada pelo cliente');
+	});
 });
 
 server.listen(3333, '127.0.0.1', () => {
-    console.log("Servidor funcionando na porta 3333");
+	console.log("Servidor funcionando na porta 3333");
 });
