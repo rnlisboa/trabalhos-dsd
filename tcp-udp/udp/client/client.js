52f41c4a720d6e7f38bfefb5ca9ce26175e09553
Ramon de Sousa Vieira <20222014040012@ifrn.local>
2024-04-01 14:57:53 -0300

    update

--- tcp-udp/udp/client/client.js
+++ tcp-udp/udp/client/client.js
@@ -4,21 +4,25 @@ const porta = 4030;
 const enderecoServidor = 'localhost';
 
 const cliente = dgram.createSocket('udp4');
+function enviarNumero() {
+    const num = parseInt(Math.random() * 1000);
+    const mensagem = Buffer.from(num.toString());
 
-const num = parseInt(Math.random() * 1000);
-const mensagem = Buffer.from(num.toString());
+    cliente.send(mensagem, porta, enderecoServidor, (err) => {
+        if (err) {
+            console.error('Erro ao enviar mensagem:', err);
+            cliente.close();
+        } else {
+            console.log('Mensagem enviada com sucesso.');
+        }
+    });
+}
 
-cliente.send(mensagem, porta, enderecoServidor, (err) => {
-    if (err) {
-        console.error('Erro ao enviar mensagem:', err);
-        cliente.close();
-    } else {
-        console.log('Mensagem enviada com sucesso.');
-    }
-});
+setInterval(enviarNumero, 1000);
+
 cliente.on('message', (mensagem, info) => {
     console.log('Mensagem recebida do servidor:', mensagem.toString());
-    console.log('Informações do servidor:', info)
+    console.log('Informações do servidor:', info);
 });
 
 cliente.on('error', (err) => {
