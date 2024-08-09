f4cd4e8d40d67465a3515aa5ba0526b601732965
rnlisboa <dev.renanlisboa@gmail.com>
2024-08-09 14:56:28 -0300

    update

--- soap/call_bot.py
+++ soap/call_bot.py
@@ -1,23 +1,21 @@
 from zeep import Client
 
-# URL do WSDL do serviço SOAP
+
 wsdl = 'https://probable-space-barnacle-xg6x7prjvp6f965r-8081.app.github.dev/ws/hello?wsdl'
 
-# Criando o cliente SOAP
+
 client = Client(wsdl=wsdl)
 
 def main():
-    # Solicita o tamanho da matriz ao usuário
+    
     tamanho = int(input("Informe o tamanho do plano cartesiano NxN: "))
 
-    # Se o tamanho for par, incrementa em 1 para torná-lo ímpar
+  
     if tamanho % 2 == 0:
         tamanho += 1
-
-    # Chama o método criaMatriz do serviço SOAP
+   
     client.service.criaMatriz(tamanho)
 
-    # Calcula a quantidade de bombas
     quantidade_bombas = (tamanho // 2) * (tamanho // 2)
     client.service.adicionaBombas(quantidade_bombas, tamanho)
 
--- soap/client.py
+++ soap/client.py
@@ -1,25 +1,25 @@
 from zeep import Client
 import uuid
 
-# URL do WSDL do serviço SOAP
+
 wsdl = 'https://probable-space-barnacle-xg6x7prjvp6f965r-8081.app.github.dev/ws/hello?wsdl'
 
-# Criando o cliente SOAP
+
 client = Client(wsdl=wsdl)
 
 def main():
-    robo_id = str(uuid.uuid4())  # Gera um ID único para o robô
+    robo_id = str(uuid.uuid4()) 
 
     try:
-        # Exibe a matriz inicial
+      
         response = client.service.printMatriz()
         print(response)
         
-        # Solicita as coordenadas do robô
+       
         y = int(input("Coordenada do eixo y: "))
         x = int(input("Coordenada do eixo x: "))
         
-        # Insere o robô na matriz
+      
         client.service.insertRobot(robo_id, y, x)
         response = client.service.printMatriz()
         print(response)
@@ -28,7 +28,7 @@ def main():
         print(e)
 
     while True:
-        # Solicita o comando de movimento ao usuário
+     
         comando = input("Qual direção deseja andar? (w: cima, s: baixo, a: esquerda, d: direita): ").strip().lower()
         if comando == 'w':
             client.service.moveUp(robo_id)
@@ -42,7 +42,7 @@ def main():
             print("Comando inválido.")
             continue
         
-        # Exibe a matriz após o movimento
+   
         response = client.service.printMatriz()
         print(response)
 
