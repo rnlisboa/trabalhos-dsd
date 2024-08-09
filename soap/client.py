fb1f46dd72458a9f235625f1b419d7e33302f2f1
rnlisboa <dev.renanlisboa@gmail.com>
2024-08-09 13:51:30 -0300

    push

--- soap/client.py
+++ soap/client.py
@@ -1,16 +1,64 @@
 from zeep import Client
 
 # URL do WSDL do serviço SOAP
-wsdl = 'https://stunning-train-gvv47pr5qgxcg96-8081.app.github.dev/ws/hello?wsdl'
+wsdl = 'https://probable-space-barnacle-xg6x7prjvp6f965r-8081.app.github.dev/ws/hello?wsdl'
 
 # Criando o cliente SOAP
 client = Client(wsdl=wsdl)
 
-# Parâmetro a ser passado para o método criaMatriz
-tamanho = 5  # Exemplo de valor
+def main():
+    # Solicita o tamanho da matriz ao usuário
+    tamanho = int(input("Informe o tamanho do plano cartesiano NxN: "))
 
-# Chama o método criaMatriz do serviço SOAP
-response = client.service.criaMatriz(tamanho)
+    # Se o tamanho for par, incrementa em 1 para torná-lo ímpar
+    if tamanho % 2 == 0:
+        tamanho += 1
 
-# Exibe a resposta
-print(response)
+    # Chama o método criaMatriz do serviço SOAP
+    client.service.criaMatriz(tamanho)
+
+    # Calcula a quantidade de bombas
+    quantidade_bombas = (tamanho // 2) * (tamanho // 2)
+    client.service.adicionaBombas(quantidade_bombas, tamanho)
+
+    try:
+        # Exibe a matriz inicial
+        response = client.service.printMatriz()
+        print(response)
+
+        # Exibe as coordenadas possíveis
+        print(f"As possíveis coordenadas vão de {tamanho // 2} a -{tamanho // 2} para x e y.")
+        
+        # Solicita as coordenadas do robô
+        y = int(input("Coordenada do eixo y: "))
+        x = int(input("Coordenada do eixo x: "))
+        
+        # Insere o robô na matriz
+        client.service.insertRobot(y, x)
+        response = client.service.printMatriz()
+        print(response)
+
+    except Exception as e:
+        print(e)
+
+    while True:
+        # Solicita o comando de movimento ao usuário
+        comando = input("Qual direção deseja andar? (w: cima, s: baixo, a: esquerda, d: direita): ").strip().lower()
+        if comando == 'w':
+            client.service.moveUp()
+        elif comando == 's':
+            client.service.moveDown()
+        elif comando == 'a':
+            client.service.moveLeft()
+        elif comando == 'd':
+            client.service.moveRight()
+        else:
+            print("Comando inválido.")
+            continue
+        
+        # Exibe a matriz após o movimento
+        response = client.service.printMatriz()
+        print(response)
+
+if __name__ == "__main__":
+    main()
